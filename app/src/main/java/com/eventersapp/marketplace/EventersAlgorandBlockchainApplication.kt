004c262b3bc60d9a35ff529e8b19f04fc916e73a
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-01-21 10:49:29 +0000

    Version 3.4.0

--- app/src/main/java/com/eventersapp/marketplace/EventersAlgorandBlockchainApplication.kt
+++ app/src/main/java/com/eventersapp/marketplace/EventersAlgorandBlockchainApplication.kt
@@ -12,4 +12,14 @@ import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
-import org.kodein.di.g
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class EventersAlgorandBlockchainApplication : Application(), KodeinAware {
+
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@EventersAlgorandBlockchainApplication))
+
+        bind() from singleton { NetworkConnectionInterceptor(instance()) }
+        bind() from singleton { ApiInterface(instance()) }
+   

52718b1837cf7fb2cf7b0a2f678728c396ab9d85
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-01-21 10:39:06 +0000

    Updated nuget package

--- app/src/main/java/com/eventersapp/marketplace/EventersAlgorandBlockchainApplication.kt
+++ app/src/main/java/com/eventersapp/marketplace/EventersAlgorandBlockchainApplication.kt
@@ -3,4 +3,13 @@ package com.eventersapp.marketplace
 import android.app.Application
 import com.eventersapp.marketplace.data.local.AppDatabase
 import com.eventersapp.marketplace.data.network.ApiInterface
-import com.eventersapp.marketplace.data.network.BlogApiInte
+import com.eventersapp.marketplace.data.network.BlogApiInterface
+import com.eventersapp.marketplace.data.network.NetworkConnectionInterceptor
+import com.eventersapp.marketplace.data.repositories.*
+import com.eventersapp.marketplace.ui.viewmodelfactory.*
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.g

990f4c6eb8810fbb1fc75df88f1e4a1d23f93278
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-01-21 09:15:44 +0000

    Update install docs

---
+++ app/src/main/java/com/eventersapp/marketplace/EventersAlgorandBlockchainApplication.kt
@@ -0,0 +1,6 @@
+package com.eventersapp.marketplace
+
+import android.app.Application
+import com.eventersapp.marketplace.data.local.AppDatabase
+import com.eventersapp.marketplace.data.network.ApiInterface
+import com.eventersapp.marketplace.data.network.BlogApiInte

ad13db6d3a3057f94b527e287f1ff0be9714da16
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-03-12 20:25:59 +0000

    v0.11.117.03+59

--- app/src/main/java/com/eventersapp/marketplace/data/model/AllEventListResponse.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/AllEventListResponse.kt
@@ -3,4 +3,22 @@ package com.eventersapp.marketplace.data.model
 
 import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
-im
+import java.io.Serializable
+
+@Keep
+data class AllEventListResponse(
+    @SerializedName("data")
+    val `data`: List<Data>
+) : Serializable {
+
+    @Keep
+    data class Data(
+        @SerializedName("event_ticket")
+        val eventTicket: ArrayList<EventTicket>,
+        @SerializedName("public_event")
+        val publicEvent: PublicEvent
+    ) : Serializable {
+
+        @Keep
+        data class EventTicket(
+            @SerializedNam

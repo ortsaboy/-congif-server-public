ce57e6740b0fa0aec8812f4126f1948c0c8852ab
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-03-31 21:12:02 +0000

    update setup file

--- app/src/main/java/com/eventersapp/marketplace/data/model/BuyResellTicketEventPostBody.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/BuyResellTicketEventPostBody.kt
@@ -2,4 +2,22 @@ package com.eventersapp.marketplace.data.model
 
 import com.google.gson.annotations.SerializedName
 
-class BuyResel
+class BuyResellTicketEventPostBody {
+    @SerializedName("data")
+    var data: Data? = null
+
+    inner class Data {
+        @SerializedName("ticket")
+        var ticket: Ticket? = null
+
+        @SerializedName("auth")
+        var auth: Auth? = null
+
+    }
+
+    inner class Auth {
+        @SerializedName("auth_type")
+        var authType: String? = null
+
+        @SerializedName("device_id")
+        var deviceId: String

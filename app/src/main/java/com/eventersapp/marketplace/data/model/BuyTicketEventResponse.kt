c14e6c93ce48ceb8561e5fcc036cba276149fa6f
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-09 17:13:13 +0000

    added intro

--- app/src/main/java/com/eventersapp/marketplace/data/model/BuyTicketEventResponse.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/BuyTicketEventResponse.kt
@@ -26,4 +26,16 @@ data class BuyTicketEventResponse(
         data class PublicEvent(
             @SerializedName("date_time")
             val dateTime: String,
-            @Serializ
+            @SerializedName("event_description")
+            val eventDescription: String,
+            @SerializedName("event_title")
+            val eventTitle: String,
+            @SerializedName("public_event_id")
+            val publicEventId: Int,
+            @SerializedName("ticket_price")
+            val ticketPrice: Int,
+            @SerializedName("total_tickets")
+            val totalTickets: Int
+        )
+    }
+}

7331ae93a66cd4f8c2c69bcb3afbe04949d0e96b
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-10 05:49:58 +0000

    adds MIT license

--- app/src/main/java/com/eventersapp/marketplace/data/model/ConnectPostBody.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/ConnectPostBody.kt
@@ -1,1 +1,19 @@
-package com.ev
+package com.eventersapp.marketplace.data.model
+
+import com.google.gson.annotations.SerializedName
+
+class ConnectPostBody {
+    @SerializedName("data")
+    var data: Data? = null
+
+    inner class Data {
+        @SerializedName("auth")
+        var auth: Auth? = null
+
+        @SerializedName("user")
+        var user: User? = null
+    }
+
+    inner class Auth {
+        @SerializedName("device_provider")
+        var

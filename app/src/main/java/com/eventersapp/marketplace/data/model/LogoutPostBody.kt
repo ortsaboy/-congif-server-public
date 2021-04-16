09e013028ce39a2867c786a852a7b7e4254c5fe7
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-16 21:43:14 +0000

    tpm: Fix NULL return in tpm_ibmvtpm_get_desired_dma

--- app/src/main/java/com/eventersapp/marketplace/data/model/LogoutPostBody.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/LogoutPostBody.kt
@@ -10,4 +10,19 @@ class LogoutPostBody {
         @SerializedName("auth")
         var auth: Auth? = null
 
-        @Seria
+        @SerializedName("user_device")
+        var userDevice: UserDevice? = null
+    }
+
+    inner class Auth {
+        @SerializedName("auth_type")
+        var authType: String? = null
+
+        @SerializedName("device_id")
+        var deviceId: String? = null
+
+        @SerializedName("token_id")
+        var tokenId: String? = null
+
+        @SerializedName("user_id")
+        var userId: Int? =

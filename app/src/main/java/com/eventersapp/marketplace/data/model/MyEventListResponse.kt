a535ad5b66d5775d41e8a5e7fe4eba8364e47fa6
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-20 07:35:28 +0000

    usb: host: xhci: use new USB_RESUME_TIMEOUT

--- app/src/main/java/com/eventersapp/marketplace/data/model/MyEventListResponse.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/MyEventListResponse.kt
@@ -8,4 +8,16 @@ import java.io.Serializable
 @Keep
 data class MyEventListResponse(
     @SerializedName("data")
-    val `data`: 
+    val `data`: ArrayList<Data>
+) : Serializable {
+    @Keep
+    data class Data(
+        @SerializedName("event_ticket")
+        val eventTicket: EventTicket,
+        @SerializedName("public_event")
+        val publicEvent: PublicEvent
+    ) : Serializable {
+        @Keep
+        data class EventTicket(
+            @SerializedName("event_ticket_id")
+            val eventTicketI

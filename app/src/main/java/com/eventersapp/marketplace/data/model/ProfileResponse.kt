c2263cbce78926cdd7c380a382ec97b784c12c8b
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-23 10:18:54 +0000

    Format cleanup

--- app/src/main/java/com/eventersapp/marketplace/data/model/ProfileResponse.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/ProfileResponse.kt
@@ -18,4 +18,14 @@ data class ProfileResponse(
         data class User(
             @SerializedName("account_address")
             val accountAddress: String,
-        
+            @SerializedName("fb_firebase_id")
+            val fbFirebaseId: String,
+            @SerializedName("fb_image_url")
+            val fbImageUrl: String,
+            @SerializedName("fb_name")
+            val fbName: String,
+            @SerializedName("fb_email")
+            val fbEmail: String,
+            @SerializedName("g_email")
+            val gEmail: String,
+            

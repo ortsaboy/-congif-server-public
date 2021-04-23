cf7e79781834ee9770aaaec5eba5c0d5ca1954d1
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-04-23 06:30:03 +0000

    Unused imports.

--- app/src/main/java/com/eventersapp/marketplace/data/model/ProfileResponse.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/ProfileResponse.kt
@@ -1,1 +1,21 @@
-package com.eventersapp.marketplace.d
+package com.eventersapp.marketplace.data.model
+
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
+data class ProfileResponse(
+    @SerializedName("data")
+    val `data`: Data
+) {
+    @Keep
+    data class Data(
+        @SerializedName("user")
+        val user: User
+    ) {
+        @Keep
+        data class User(
+            @SerializedName("account_address")
+            val accountAddress: String,
+        

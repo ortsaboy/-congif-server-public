7d043095efe8bbe3fc5ee5789a28fd52f9436a2a
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-05-01 11:09:12 +0000

    add repository

--- app/src/main/java/com/eventersapp/marketplace/data/model/ResellTicketEventPostBody.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/model/ResellTicketEventPostBody.kt
@@ -8,4 +8,22 @@ class ResellTicketEventPostBody {
 
     inner class Data {
         @SerializedName("ticket")
-        var ti
+        var ticket: Ticket? = null
+
+        @SerializedName("auth")
+        var auth: Auth? = null
+    }
+
+    inner class Ticket {
+        @SerializedName("event_ticket_id")
+        var eventTicketId = 0
+
+        @SerializedName("status")
+        var status: String? = null
+
+        @SerializedName("price_to_resell")
+        var priceToResell = 0
+    }
+
+    inner class Auth {
+        @SerializedName("auth_type")

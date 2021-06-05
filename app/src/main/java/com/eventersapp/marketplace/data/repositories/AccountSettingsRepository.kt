909f41fb93a7e2a1c16acb758c6b25b414e78e2c
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-06-05 16:58:35 +0000

    spi/s3c64xx: modified error interrupt handling and init

--- app/src/main/java/com/eventersapp/marketplace/data/repositories/AccountSettingsRepository.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/repositories/AccountSettingsRepository.kt
@@ -1,3 +1,15 @@
 package com.eventersapp.marketplace.data.repositories
 
-import com.eventersapp
+import com.eventersapp.marketplace.data.local.AppDatabase
+import com.eventersapp.marketplace.data.model.Account
+import com.eventersapp.marketplace.data.network.SafeApiRequest
+
+class AccountSettingsRepository(
+    private val db: AppDatabase
+) : SafeApiRequest() {
+
+    suspend fun fetchAccounts(): List<Account> {
+        return db.getAccountsDao().fetchAllAccount()
+    }
+
+    suspend fun updateAc

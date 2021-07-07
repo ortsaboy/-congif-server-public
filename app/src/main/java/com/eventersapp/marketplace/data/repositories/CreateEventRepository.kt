f1a1c360a852c01b9e3f8bb15186d0023fc98d35
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-07-07 20:09:51 +0000

    Linux 3.4.82

--- app/src/main/java/com/eventersapp/marketplace/data/repositories/CreateEventRepository.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/repositories/CreateEventRepository.kt
@@ -1,4 +1,14 @@
 package com.eventersapp.marketplace.data.repositories
 
 import com.eventersapp.marketplace.data.model.CreateEventPostBody
-imp
+import com.eventersapp.marketplace.data.model.CreateEventResponse
+import com.eventersapp.marketplace.data.network.ApiInterface
+import com.eventersapp.marketplace.data.network.SafeApiRequest
+
+class CreateEventRepository(
+    private val api: ApiInterface
+) : SafeApiRequest() {
+
+    suspend fun createEvent(
+        createEventPostBody: CreateEventPostBody
+    ): CreateEventRespons

7225fcca5550955b71d3b2316535eea78681bb17
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-08-03 07:08:54 +0000

    ignore .vscode

--- app/src/main/java/com/eventersapp/marketplace/data/repositories/SignupRepository.kt
+++ app/src/main/java/com/eventersapp/marketplace/data/repositories/SignupRepository.kt
@@ -3,4 +3,13 @@ package com.eventersapp.marketplace.data.repositories
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.eventersapp.marketplace.data.model
+import com.eventersapp.marketplace.data.model.ConnectPostBody
+import com.eventersapp.marketplace.data.model.ConnectResponse
+import com.eventersapp.marketplace.data.network.ApiInterface
+import com.eventersapp.marketplace.data.network.SafeApiRequest
+import com.eventersapp.marketplace.util.Event
+import com.eventersapp.marketplace.util.State
+import com.facebook.AccessToken
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.au

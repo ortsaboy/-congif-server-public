27f3637666c6615f190aed207ab5ad4225453814
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-09-03 07:41:41 +0000

    ASoC: sgtl5000: Fix SMALL_POP bit definition

---
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/AddNewAccountFragment.kt
@@ -0,0 +1,10 @@
+package com.eventersapp.marketplace.ui.accountsettings
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigatio

dbdc78a0eb8a152636e1ffc6fe50e95f50a983bb
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-09-03 16:28:13 +0000

    dimagimon

--- app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/AddNewAccountFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/AddNewAccountFragment.kt
@@ -7,4 +7,18 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigatio
+import androidx.navigation.fragment.findNavController
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.FragmentAddNewAccountBinding
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+
+
+class AddNewAccountFragment : Fragment(), KodeinAware, View.OnClickListener {
+
+
+    override val kodein by closestKodein()
+    private lateinit var dataBind: FragmentAddNewAccountBinding
+
+
+    over

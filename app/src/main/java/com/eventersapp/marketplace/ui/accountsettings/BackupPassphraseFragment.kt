e22d9d177851fe01fbb4fdc2c8d41df0aa4c151c
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-09-09 12:01:10 +0000

    updated cdb

--- app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/BackupPassphraseFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/BackupPassphraseFragment.kt
@@ -8,4 +8,12 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.GridLayoutManager
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.FragmentBackupPassphraseBinding
+import 

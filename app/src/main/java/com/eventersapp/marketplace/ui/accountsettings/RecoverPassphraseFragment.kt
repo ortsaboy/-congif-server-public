562fb93c6b290ba01475dec580e7765261167392
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-09-30 20:25:12 +0000

    Release 6.2.0

--- app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/RecoverPassphraseFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/RecoverPassphraseFragment.kt
@@ -2,4 +2,13 @@ package com.eventersapp.marketplace.ui.accountsettings
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import a
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.da

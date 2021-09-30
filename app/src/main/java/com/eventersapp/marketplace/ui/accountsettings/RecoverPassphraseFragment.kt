fc290696bb6f7d10a66e01a01a7eb03c1b771d9e
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-09-30 21:34:08 +0000

    Fix the changelog

--- app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/RecoverPassphraseFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/RecoverPassphraseFragment.kt
@@ -11,4 +11,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.eventersapp.marketplace.R
-import com.eventersapp.marketplace.da
+import com.eventersapp.marketplace.databinding.FragmentRecoverPassphraseBinding
+import com.eventersapp.marketplace.ui.viewmodel.RecoverPassphraseViewModel
+import com.eventersapp.marketplace.ui.viewmodelfactory.RecoverPassphraseViewModelFactory
+import com.eventersapp.marketplace.util.EventObserver
+import com.eventersapp.marketplace.util.snackbar
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodei

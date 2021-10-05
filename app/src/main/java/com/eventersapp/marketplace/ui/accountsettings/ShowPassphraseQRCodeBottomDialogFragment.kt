79a7a27a17be85f8d7ad4d6c1fa485d7de91cc87
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-10-05 23:09:49 +0000

    Update sphinx from 1.6.1 to 1.6.2

--- app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/ShowPassphraseQRCodeBottomDialogFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/accountsettings/ShowPassphraseQRCodeBottomDialogFragment.kt
@@ -14,4 +14,15 @@ import androidx.databinding.DataBindingUtil
 import com.eventersapp.marketplace.R
 import com.eventersapp.marketplace.databinding.ShowPassphraseQrCodeBottomSheetBinding
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.googl
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import com.journeyapps.barcodescanner.BarcodeEncoder
+
+
+class ShowPassphraseQRCodeBottomDialogFragment : BottomSheetDialogFragment(),
+    View.OnClickListener {
+
+    private late

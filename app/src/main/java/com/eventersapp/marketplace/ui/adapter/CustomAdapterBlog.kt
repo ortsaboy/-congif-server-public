7ccaceb7e7394c99aa1636d903c53ade8f7b2112
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-10-22 12:10:45 +0000

    Fix ui files

--- app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterBlog.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterBlog.kt
@@ -1,4 +1,13 @@
 package com.eventersapp.marketplace.ui.adapter
 
 import android.annotation.SuppressLint
-import android.view.LayoutIn
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.data.model.BlogListResponse
+import com.eventersapp.marketplace.databinding.ListItemBlogPostsBinding
+import com.eventersapp.marketplace.util.AppUtils
+import com.e

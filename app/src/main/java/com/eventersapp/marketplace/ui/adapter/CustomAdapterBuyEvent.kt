63612b8da08e907d11af7e609846efd7cd73c2c0
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-10-25 16:14:41 +0000

    fn

--- app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterBuyEvent.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterBuyEvent.kt
@@ -8,4 +8,12 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.eventersapp.marketplace.R
 import com.eventersapp.marketplace.data.model.AllEventListResponse
-imp
+import com.eventersapp.marketplace.databinding.ListItemBuyEventBinding
+import com.eventersapp.marketplace.ui.blockchain.BuyEventFragment
+import com.eventersapp.marketplace.util.AppConstants
+import com.eventersapp.marketplace.util.LoadingViewHolder
+
+class CustomAdapterBuyEvent(private val fragment: BuyEventFragment) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    comp

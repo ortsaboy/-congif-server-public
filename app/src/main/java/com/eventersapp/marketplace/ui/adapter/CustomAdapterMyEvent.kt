fb55b5f0be354d56722f7e282c5cbad05cfe1a33
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-10-30 08:40:42 +0000

    Moved code around

--- app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterMyEvent.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/adapter/CustomAdapterMyEvent.kt
@@ -42,3 +42,75 @@ class CustomAdapterMyEvent : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
                 LoadingViewHolder(view)
             }
         }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ItemViewHolder -> {
+                myEventList[position]?.let { holder.bindItems(it) }
+            }
+            is LoadingViewHolder -> {
+                holder.showLoadingView()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return myEventList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            myEventList[position] == null -> TYPE_LOADING
+            else -> TYPE_ITEM
+        }
+    }
+
+    fun setData(newMyEvent: ArrayList<MyEventListResponse.Data?>?) {
+        if (newMyEvent != null) {
+            myEventList.clear()
+            myEventList.addAll(newMyEvent)
+        } else {
+            myEventList.add(newMyEvent)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun getListData() = myEventList
+
+    inner class ItemViewHolder(val binding: ListItemMyEventBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bindItems(myEvent: MyEventListResponse.Data) {
+            binding.apply {
+                textEventIdDateTime.text =
+                    "#${myEvent.eventTicket.eventTicketId} | ${
+                        AppUtils.changeDateFormat(
+                            myEvent.publicEvent.dateTime,
+                            DISPLAY_DATE_FORMAT,
+                            API_DATE_FORMAT
+                        )
+                    }"
+                textTicketPrice.text = "$ ${myEvent.eventTicket.price}"
+                textEventTitle.text = myEvent.publicEvent.eventTitle
+                textEventDescription.text =
+                    myEvent.publicEvent.eventDescription
+                if (myEvent.eventTicket.status == ACTIVE) {
+                    textStatus.setTextColor(Color.parseColor("#4CAF50"))
+                } else {
+                    textStatus.setTextColor(Color.parseColor("#FF9800"))
+                }
+                textStatus.text = myEvent.eventTicket.status
+                if (myEvent.publicEvent.eventImage != null && myEvent.publicEvent.eventImage.isNotEmpty()) {
+                    imageEventImage.visibility = View.VISIBLE
+                    AppUtils.setGlideImage(imageEventImage, myEvent.publicEvent.eventImage)
+                } else {
+                    imageEventImage.visibility = View.GONE
+                }
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/eventersapp/marketplace/ui/blockchain/AllEventFragment.kt
@@ -0,0 +1,8 @@
+package com.eventersapp.marketplace.ui.blockchain
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.d

97448ebc9529d25780392b7b667e6b1084d6d3b4
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-10-30 14:55:11 +0000

    [maven-release-plugin] prepare release v2.1.1

--- app/src/main/java/com/eventersapp/marketplace/ui/blockchain/AllEventFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/blockchain/AllEventFragment.kt
@@ -5,4 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
-import androidx.d
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.FragmentAllEventBinding
+import com.eventersap

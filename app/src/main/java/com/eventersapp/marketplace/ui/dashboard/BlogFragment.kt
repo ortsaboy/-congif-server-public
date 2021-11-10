d8291f758ce6f4da0d51e41882a68bd8202602d3
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-11-10 09:31:45 +0000

    version 1.3.2

---
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/BlogFragment.kt
@@ -0,0 +1,8 @@
+package com.eventersapp.marketplace.ui.dashboard
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databi

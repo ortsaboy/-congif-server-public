61f33c129d40bdac53dc14ce03361d0d4c4db9b4
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-11-10 16:31:04 +0000

    Fixed NPE.

--- app/src/main/java/com/eventersapp/marketplace/ui/dashboard/BlogFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/BlogFragment.kt
@@ -5,4 +5,13 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databi
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.FragmentBlogBinding
+impo

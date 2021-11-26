477a32e53b7a50178f2135598bac29ada6d20778
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-11-26 08:49:46 +0000

    Initial commit

--- app/src/main/java/com/eventersapp/marketplace/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/DashboardFragment.kt
@@ -1,3 +1,13 @@
 package com.eventersapp.marketplace.ui.dashboard
 
-import android.content.res.Col
+import android.content.res.ColorStateList
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController

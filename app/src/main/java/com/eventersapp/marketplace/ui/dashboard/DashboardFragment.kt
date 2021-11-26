ca80721d7e629e718831585460897ce05daf02a6
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-11-26 20:11:13 +0000

    updated setup file

--- app/src/main/java/com/eventersapp/marketplace/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/DashboardFragment.kt
@@ -11,3 +11,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.DashboardFragmentBinding
+import com.eventersapp.marketplace.ui.viewmodel.DashboardViewModel
+import com.eventersapp.marketplace.util.color
+
+
+class DashboardFragment : Fragment(), View.OnClickListener {
+
+    companion object {
+        fun newInstance() = DashboardFragment()
+    }
+
+    private lateinit var dataBind

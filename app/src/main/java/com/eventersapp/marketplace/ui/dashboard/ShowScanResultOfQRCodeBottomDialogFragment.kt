f8cbaa40b00cf8cf3cd39bc14b083ac62efe7486
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-12-07 06:38:25 +0000

    Catch exceptions

--- app/src/main/java/com/eventersapp/marketplace/ui/dashboard/ShowScanResultOfQRCodeBottomDialogFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/ShowScanResultOfQRCodeBottomDialogFragment.kt
@@ -4,4 +4,11 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.
+import android.view.ViewGroup
+import androidx.annotation.Nullable
+import androidx.databinding.DataBindingUtil
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.ShowScanResultOfQrCodeBottomSheetBinding
+import com.eventersapp.marketplace.util.copyToClipboard
+import com.eventersapp.marketplace.util.showToast
+import com.google.android.material.bottomsheet.BottomSheetD

65360128e63d3328fb2e9866d6717cbfcb0071e3
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-12-07 12:41:17 +0000

    delete project

--- app/src/main/java/com/eventersapp/marketplace/ui/dashboard/ShowScanResultOfQRCodeBottomDialogFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/dashboard/ShowScanResultOfQRCodeBottomDialogFragment.kt
@@ -11,4 +11,17 @@ import com.eventersapp.marketplace.R
 import com.eventersapp.marketplace.databinding.ShowScanResultOfQrCodeBottomSheetBinding
 import com.eventersapp.marketplace.util.copyToClipboard
 import com.eventersapp.marketplace.util.showToast
-import com.google.android.material.bottomsheet.BottomSheetD
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class ShowScanResultOfQRCodeBottomDialogFragment : BottomSheetDialogFragment(),
+    View.OnClickListener {
+
+    companion object {
+        fun newInstance(): ShowScanResultOfQRCodeBottomDialogFragment {
+            return ShowScanResultOfQRCodeBottomDialogFragment()
+        }
+    }
+
+    private lateinit var dataBind: Show

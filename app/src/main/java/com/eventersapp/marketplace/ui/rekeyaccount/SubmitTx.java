12a269558996349c2baa1932753d44021cb05f0f
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2021-12-25 13:45:50 +0000

    Update language file

--- app/src/main/java/com/eventersapp/marketplace/ui/rekeyaccount/SubmitTx.java
+++ app/src/main/java/com/eventersapp/marketplace/ui/rekeyaccount/SubmitTx.java
@@ -4,4 +4,11 @@ import android.util.Log;
 
 import com.algorand.algosdk.account.Account;
 import com.algorand.algosdk.algod.client.AlgodClient;
-import com.algorand.algo
+import com.algorand.algosdk.algod.client.ApiException;
+import com.algorand.algosdk.algod.client.api.AlgodApi;
+import com.algorand.algosdk.algod.client.auth.ApiKeyAuth;
+import com.algorand.algosdk.algod.client.model.NodeStatus;
+import com.algorand.algosdk.algod.client.model.TransactionID;
+import com.algorand.algosdk.algod.client.model.TransactionParams;
+import com.algorand.algosdk.crypto.Address;
+import com.algorand.algosdk.cry

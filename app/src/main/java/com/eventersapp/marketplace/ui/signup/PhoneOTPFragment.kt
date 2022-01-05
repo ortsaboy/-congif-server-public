05004287c04a2e259c95c641a1c375614acfab0c
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-01-05 13:14:10 +0000

    comment style

--- app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
@@ -1,4 +1,14 @@
 package com.eventersapp.marketplace.ui.signup
 
 import android.os.Bundle
-impor
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.PhoneOtpFragmentBi

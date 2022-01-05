f44d1f1054596471cba75a36454e9021eaec109f
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-01-05 22:43:35 +0000

    Bump version to 1.9.0

--- app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
@@ -11,4 +11,16 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.eventersapp.marketplace.R
-import com.eventersapp.marketplace.databinding.PhoneOtpFragmentBi
+import com.eventersapp.marketplace.databinding.PhoneOtpFragmentBinding
+import com.eventersapp.marketplace.ui.viewmodel.PhoneOTPViewModel
+import com.eventersapp.marketplace.ui.viewmodelfactory.PhoneOTPViewModelFactory
+import com.eventersapp.marketplace.util.*
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
+
+
+class PhoneOTPFragment : Fragment(), KodeinAware {
+
+    override val kodein by closestKodein()
+    priv

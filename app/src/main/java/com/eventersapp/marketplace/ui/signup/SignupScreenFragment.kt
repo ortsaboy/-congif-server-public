33785860daff28e87302d822e21cbe14bc5d0ef0
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-01-19 16:25:24 +0000

    add zsh

--- app/src/main/java/com/eventersapp/marketplace/ui/signup/SignupScreenFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/signup/SignupScreenFragment.kt
@@ -9,4 +9,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.fin
+import androidx.navigation.fragment.findNavController
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.databinding.SignupScreenFragmentBinding
+import com.eventersapp.marketplace.ui.viewmodel.SignupViewModel
+import com.eventersapp.marketplace.ui.viewmodelfactory.SignupViewModelFactory
+import com.eventersapp.marketplace.util.*
+import com.eventersapp.marketplace.util.AppConstants.FACEBOOK
+import com.eventersapp.marketplace.util

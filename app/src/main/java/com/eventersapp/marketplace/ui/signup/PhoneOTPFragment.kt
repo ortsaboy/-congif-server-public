492b9133a38090f5bde41ef5324bf047381db1c0
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-01-19 07:01:41 +0000

    mnt: Update unprivileged remount test

--- app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/signup/PhoneOTPFragment.kt
@@ -219,4 +219,14 @@ class PhoneOTPFragment : Fragment(), KodeinAware {
             }
         })
         dataBind.inputOtpBox5.addTextChangedListener(object : TextWatcher {
-       
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+                if (editable.length == 1)
+                    dataBind.inputOtpBox6.requestFocus()
+            }
+      

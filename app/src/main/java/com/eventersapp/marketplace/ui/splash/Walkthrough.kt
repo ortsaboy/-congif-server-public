70fbaf72b08bf8eefc1f51b6c62a1dcda990b70c
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-02-09 10:46:54 +0000

    Cleanup README.

--- app/src/main/java/com/eventersapp/marketplace/ui/splash/Walkthrough.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/splash/Walkthrough.kt
@@ -15,4 +15,13 @@ class Walkthrough : OnboarderActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         onboarderPages = ArrayList()
-        val on
+        val onboarderPage1 =
+            OnboarderPage(
+                "Event App",
+                "All event suites like guest management, invites, website and app in one place. It is part of Organiser toolkit",
+                R.drawable.onboard_mobile_img
+            )
+        val onboarderPage2 =
+            OnboarderPage(
+                "Personalised Website",
+                "Your stories and memories in one place

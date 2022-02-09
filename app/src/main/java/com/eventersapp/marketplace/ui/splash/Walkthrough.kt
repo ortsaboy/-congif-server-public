7a068f67c85bf2ebe2e86d73fefd62811217cd70
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-02-09 06:11:49 +0000

    Updated version history

--- app/src/main/java/com/eventersapp/marketplace/ui/splash/Walkthrough.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/splash/Walkthrough.kt
@@ -2,4 +2,17 @@ package com.eventersapp.marketplace.ui.splash
 
 import android.os.Bundle
 import com.chyrta.onboarder.OnboarderActivity
-import com.chyrta
+import com.chyrta.onboarder.OnboarderPage
+import com.eventersapp.marketplace.HostActivity
+import com.eventersapp.marketplace.R
+import com.eventersapp.marketplace.util.goToActivity
+
+
+class Walkthrough : OnboarderActivity() {
+
+    private var onboarderPages: List<OnboarderPage>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        onboarderPages = ArrayList()
+        val on

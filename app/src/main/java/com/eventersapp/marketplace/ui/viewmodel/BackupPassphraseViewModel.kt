55659e9de746dfae6955ce814c2d096a423dd550
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-02-17 08:34:13 +0000

    Run scan-local.sh on haskell:...

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/BackupPassphraseViewModel.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/BackupPassphraseViewModel.kt
@@ -9,4 +9,17 @@ import androidx.navigation.Navigation
 import com.algorand.algosdk.account.Account
 import com.eventersapp.marketplace.R
 import com.eventersapp.marketplace.util.Event
-import org.bouncyca
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.Security
+
+class BackupPassphraseViewModel : ViewModel() {
+
+    var question = MutableLiveData<String>()
+    var selectPassPhrase = MutableLiveData<String>()
+    private var questionCount = 0
+    private var randomNumber = 0
+    private var passphraseStr = ""
+    private var accountAddress = ""
+
+    private val _messageLiveData = MutableLiveData<Event<String>>()
+    val mes

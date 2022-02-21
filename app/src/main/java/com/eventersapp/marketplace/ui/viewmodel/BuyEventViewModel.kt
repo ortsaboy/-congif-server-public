a7204df45dd44c7b648e01d03709f406de1a54cf
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-02-21 17:59:16 +0000

    correct style

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/BuyEventViewModel.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/BuyEventViewModel.kt
@@ -4,4 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-impo
+import com.eventersapp.marketplace.data.model.*
+import com.eventersapp.marketplace.data.repositories.BuyEventRepository
+import com.eventersapp.marketplace.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class BuyEventViewModel(private val repository: BuyEventRepository) : ViewModel() {
+
+
+    private var fireb

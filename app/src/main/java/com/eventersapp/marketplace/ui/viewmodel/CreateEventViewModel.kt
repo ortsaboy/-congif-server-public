742b12643cdb52b5bfcbed3a7988eeefa7048366
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-03-04 19:05:53 +0000

    First public commit

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/CreateEventViewModel.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/CreateEventViewModel.kt
@@ -1,1 +1,10 @@
-packa
+package com.eventersapp.marketplace.ui.viewmodel
+
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eventersapp.marketplace.data.model.ConnectResponse
+import com.eventersapp.marketplace.data.model.CreateEventPostBody
+import com.eventersapp.marketplac

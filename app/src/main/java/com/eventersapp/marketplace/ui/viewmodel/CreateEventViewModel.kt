03e0187f67e7cb00af21721f35594351da72c694
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-03-07 10:40:33 +0000

    Update change log and version

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/CreateEventViewModel.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/CreateEventViewModel.kt
@@ -7,4 +7,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.eventersapp.marketplace.data.model.ConnectResponse
 import com.eventersapp.marketplace.data.model.CreateEventPostBody
-import com.eventersapp.marketplac
+import com.eventersapp.marketplace.data.model.CreateEventResponse
+import com.eventersapp.marketplace.data.repositories.CreateEventRepository
+import com.eventersapp.marketplace.util.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class CreateEventViewModel(private val repository: CreateEventRepository) : ViewModel() {
+
+    companion object {
+        const val ONLINE = "online"
+  

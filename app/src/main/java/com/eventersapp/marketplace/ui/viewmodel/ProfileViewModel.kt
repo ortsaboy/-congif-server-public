3f458b5fc607203f8766e2b14b1ae9a7c0ac55ce
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-03-30 16:51:12 +0000

    Cleaned up project

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodel/ProfileViewModel.kt
@@ -3,4 +3,11 @@ package com.eventersapp.marketplace.ui.viewmodel
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-i
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eventersapp.marketplace.data.model.LogoutPostBody
+import com.eventersapp.marketplace.data.model.LogoutResponse
+import com.eventersapp.marketplace.data.model.ProfileResponse
+import com.eventersapp.marketplace.data.repositories.ProfileRepository
+import com.eventersapp.marketplace.util.*
+import com.evente

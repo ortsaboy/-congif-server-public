535be37ab95335005c15af154b6b39baeab50ffb
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-04-19 22:18:09 +0000

    exercises

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/AccountDetailsViewModelFactory.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/AccountDetailsViewModelFactory.kt
@@ -5,4 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.eventersapp.marketplace.data.repositories.AccountDetailsRepository
 import com.eventersapp.marketplace.ui.viewmodel.AccountDetailsViewModel
 
-@Suppress("UNCHECKED_CAST"
+@Suppress("UNCHECKED_CAST")
+class AccountDetailsViewModelFactory(
+    private val repository: AccountDetailsRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return AccountDetailsViewModel(repository) as T
+    }
+}
---
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/AccountSettingsViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.eventersapp.marketplace.ui.viewmodelfactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewM

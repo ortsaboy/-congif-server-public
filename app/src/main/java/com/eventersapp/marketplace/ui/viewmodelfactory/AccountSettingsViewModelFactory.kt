11003d64ba0cc068140f58b17d32ff93175d7660
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-04-21 07:20:31 +0000

    Added repository to package.json

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/AccountSettingsViewModelFactory.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/AccountSettingsViewModelFactory.kt
@@ -1,4 +1,14 @@
 package com.eventersapp.marketplace.ui.viewmodelfactory
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewM
+import androidx.lifecycle.ViewModelProvider
+import com.eventersapp.marketplace.data.repositories.AccountSettingsRepository
+import com.eventersapp.marketplace.ui.viewmodel.AccountSettingsViewModel
+
+@Suppress("UNCHECKED_CAST")
+class AccountSettingsViewModelFactory(
+    private val repository: AccountSettingsRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        r

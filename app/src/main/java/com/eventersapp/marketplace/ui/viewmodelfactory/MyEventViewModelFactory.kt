8bd5c11a336e305f4232ed805188102aff30d20f
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-04-26 13:13:01 +0000

    Updated project version after build [skip ci]

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/MyEventViewModelFactory.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/MyEventViewModelFactory.kt
@@ -1,1 +1,13 @@
-package com.eventersapp.marketplace.ui.view
+package com.eventersapp.marketplace.ui.viewmodelfactory
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.eventersapp.marketplace.data.repositories.MyEventRepository
+import com.eventersapp.marketplace.ui.viewmodel.MyEventViewModel
+
+@Suppress("UNCHECKED_CAST")
+class MyEventViewModelFactory(
+    private val repository: MyEventRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+ 

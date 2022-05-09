368da1b1b9002c904aa624275193b75e9cfae09f
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-05-09 13:58:36 +0000

    Debugging output

--- app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/ProfileViewModelFactory.kt
+++ app/src/main/java/com/eventersapp/marketplace/ui/viewmodelfactory/ProfileViewModelFactory.kt
@@ -5,4 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.eventersapp.marketplace.data.repositories.ProfileRepository
 import com.eventersapp.marketplace.ui.viewmodel.ProfileViewModel
 
-@S
+@Suppress("UNCHECKED_CAST")
+class ProfileViewModelFactory(
+    private val repository: ProfileRepository
+) : ViewModelProvider.NewInstanceFactory() {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ProfileViewModel(repository) as T
+    }
+}

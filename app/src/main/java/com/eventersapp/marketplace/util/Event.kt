a63e61d90d70882f24b87c421c952bd122a64a68
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-05-21 09:38:53 +0000

    Fix flake8 errors.

--- app/src/main/java/com/eventersapp/marketplace/util/Event.kt
+++ app/src/main/java/com/eventersapp/marketplace/util/Event.kt
@@ -10,4 +10,23 @@ open class Event<out T>(private val content: T) {
      * @return The unconsumed content or `null` if it was consumed already.
      */
     fun consume(): T? {
-        return if (con
+        return if (consumed) {
+            null
+        } else {
+            consumed = true
+            content
+        }
+    }
+
+    /**
+     * @return The content whether it's been handled or not.
+     */
+    fun peek(): T = content
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Event<*>
+
+        if (content != ot

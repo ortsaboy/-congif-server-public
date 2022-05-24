4eda15921164560400b1c61b4c66207b54f0a840
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-05-24 19:52:03 +0000

    smd_pkt: Add sanity check to avoid unregistering the driver twice

--- app/src/main/java/com/eventersapp/marketplace/util/ProgressBar.kt
+++ app/src/main/java/com/eventersapp/marketplace/util/ProgressBar.kt
@@ -1,4 +1,20 @@
 package com.eventersapp.marketplace.util
 
 import android.annotation.SuppressLint
-import android.app.Di
+import android.app.Dialog
+import android.content.Context
+import android.view.Window
+import android.view.WindowManager
+import com.eventersapp.marketplace.R
+
+class ProgressBar {
+    private var dialog: Dialog? = null
+
+    companion object {
+
+        @SuppressLint("StaticFieldLeak")
+        private var gifProgress: ProgressBar? = null
+
+        fun getInstance(): ProgressBar {
+            if (gifProgress == null) {
+          

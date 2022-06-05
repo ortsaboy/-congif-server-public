99eca6b00a37939f0a37e0cb0a5e4b41afa06786
ortsaboy <92518795+ortsaboy@users.noreply.github.com>
2022-06-05 13:52:13 +0000

    fixed typos in README

--- app/src/main/java/com/eventersapp/marketplace/util/SharedPref.kt
+++ app/src/main/java/com/eventersapp/marketplace/util/SharedPref.kt
@@ -6,4 +6,18 @@ import com.google.gson.Gson
 object SharedPref {
 
     private const val EVENTERS_PREFERENCE = "EventersAlgorandBlockchainPreference"
-    //<editor-fold desc="Prefer
+    //<editor-fold desc="Preference Keys">
+
+    const val KEY_USER_DATA = "user_data"
+    const val KEY_USER_ID = "user_id"
+    const val KEY_PROVIDER = "provider"
+
+    //</editor-fold>
+
+    //<editor-fold desc="Default Values">
+    private const val NO_VALUE_PREF = ""
+    private const val NO_VALUE = 0
+    private const val NO_VALUE_LONG = 0L
+    //</editor-fold>
+
+    //<editor-fold desc="Get/Set/Clear Pr
